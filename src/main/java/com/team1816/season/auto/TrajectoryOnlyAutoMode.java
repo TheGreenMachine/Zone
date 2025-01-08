c11e8df78d552f21f1b35ccab6c14281c817a762
EndigoSkull <137455719+EndigoSkull@users.noreply.github.com>
2025-01-07 19:22:00 -0600

    base commit, all code should work for other years if want to implement

--- src/main/java/com/team1816/season/auto/TrajectoryOnlyAutoMode.java
+++ src/main/java/com/team1816/season/auto/TrajectoryOnlyAutoMode.java
@@ -2,16 +2,26 @@ package com.team1816.season.auto;
 
 import com.team1816.core.states.RobotState;
 import com.team1816.lib.auto.AutoModeEndedException;
+import com.team1816.lib.auto.actions.AutoAction;
+import com.team1816.lib.auto.actions.ParallelAction;
+import com.team1816.lib.auto.actions.SeriesAction;
+import com.team1816.lib.auto.actions.TrajectoryAction;
 import com.team1816.lib.auto.modes.AutoMode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TrajectoryOnlyAutoMode extends AutoMode {
     private RobotState rs;
 
     public TrajectoryOnlyAutoMode(RobotState rs){
+        super(rs.dynamicAutoScript2025.getAutoTrajectoryActions());
         this.rs = rs;
     }
 
     @Override
     protected void routine() throws AutoModeEndedException {
+        for(int i = 0; i < trajectoryActions.size(); i++)
+            runAction(trajectoryActions.get(i));
     }
 }
