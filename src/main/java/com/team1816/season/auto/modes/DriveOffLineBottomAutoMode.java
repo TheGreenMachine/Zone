61dfa29f08ef61485d15eb581d4df78817f449b6
Awesome65432139 <104233315+AWESOME654321@users.noreply.github.com>
2025-02-15 09:22:07 -0600

    add bottom and top path

---
+++ src/main/java/com/team1816/season/auto/modes/DriveOffLineBottomAutoMode.java
@@ -0,0 +1,39 @@
+package com.team1816.season.auto.modes;
+
+import com.team1816.lib.auto.AutoModeEndedException;
+import com.team1816.lib.auto.Color;
+import com.team1816.lib.auto.actions.SeriesAction;
+import com.team1816.lib.auto.actions.TrajectoryAction;
+import com.team1816.lib.auto.modes.AutoMode;
+import com.team1816.season.auto.path.DriveOffLineBottomPath;
+import com.team1816.season.auto.path.DriveOffLineMiddlePath;
+import edu.wpi.first.math.geometry.Pose2d;
+
+import java.util.List;
+
+public class DriveOffLineBottomAutoMode extends AutoMode {
+
+    public DriveOffLineBottomAutoMode(Color color) {
+        super(
+                List.of(
+                        new TrajectoryAction(
+                                new DriveOffLineBottomPath(color)
+                        )
+                )
+        );
+    }
+
+    @Override
+    protected void routine() throws AutoModeEndedException {
+        runAction(
+                new SeriesAction(
+                        trajectoryActions.get(0)
+                )
+        );
+    }
+
+    @Override
+    public Pose2d getInitialPose() {
+        return trajectoryActions.get(0).getTrajectory().getInitialPose();
+    }
+}
---
+++ src/main/java/com/team1816/season/auto/path/DriveOffLineBottomPath.java
@@ -0,0 +1,34 @@
+package com.team1816.season.auto.path;
+
+import com.team1816.lib.auto.Color;
+import com.team1816.lib.auto.paths.AutoPath;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+import java.util.List;
+
+public class DriveOffLineBottomPath extends AutoPath {
+    public DriveOffLineBottomPath(Color color) {
+        super(color);
+    }
+    @Override
+    protected List<Pose2d> getWaypoints() {
+        return List.of(
+                new Pose2d(7.54, 0.93, Rotation2d.fromDegrees(180)),
+                new Pose2d(5.85, 0.93, Rotation2d.fromDegrees(180))
+        );
+    }
+
+    @Override
+    protected List<Rotation2d> getWaypointHeadings() {
+        return List.of(
+                Rotation2d.fromDegrees(180),
+                Rotation2d.fromDegrees(180)
+        );
+    }
+
+    @Override
+    protected boolean usingApp() {
+        return true;
+    }
+}
