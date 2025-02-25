20319bbc0f089af96229ec28ab30c83dbd00b198
chococaker <152650318+chococaker@users.noreply.github.com>
2025-02-24 18:57:06 -0600

    Make straighter autos

--- src/main/java/com/team1816/season/auto/path/BottomToSideThree.java
+++ src/main/java/com/team1816/season/auto/path/BottomToSideThree.java
@@ -15,6 +15,7 @@ public class BottomToSideThree extends AutoPath {
     protected List<Pose2d> getWaypoints() {
         return List.of(
                 new Pose2d(7.12, 0.53, Rotation2d.fromDegrees(90)),
+                new Pose2d(6.41, 1.70, Rotation2d.fromDegrees(130)),
                 new Pose2d(5.24, 2.86, Rotation2d.fromDegrees(129))
         );
     }
@@ -23,6 +24,7 @@ public class BottomToSideThree extends AutoPath {
     protected List<Rotation2d> getWaypointHeadings() {
         return List.of(
                 Rotation2d.fromDegrees(90),
+                Rotation2d.fromDegrees(130),
                 Rotation2d.fromDegrees(120)
         );
     }
