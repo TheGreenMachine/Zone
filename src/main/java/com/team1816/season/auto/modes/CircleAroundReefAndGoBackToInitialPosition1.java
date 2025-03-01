40e861416fd4c46fda5c5b2c8e27e5211a8f63d8
chococaker <152650318+chococaker@users.noreply.github.com>
2025-02-28 21:03:50 -0600

    Switch to (presumably) accurate initial path pose

--- src/main/java/com/team1816/season/auto/modes/CircleAroundReefAndGoBackToInitialPosition1.java
+++ src/main/java/com/team1816/season/auto/modes/CircleAroundReefAndGoBackToInitialPosition1.java
@@ -5,19 +5,19 @@ import com.team1816.lib.auto.actions.*;
 import com.team1816.lib.auto.modes.AutoMode;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 
 public class CircleAroundReefAndGoBackToInitialPosition1 extends AutoMode {
+    private final PathPlannerAction action = new PathPlannerAction("Circle Around Reef And Go Back To Initial Position 1");
+
     @Override
     protected void routine() throws AutoModeEndedException {
         runAction(
-                new PathPlannerAction("Circle Around Reef And Go Back To Initial Position 1")
+                action
         );
     }
 
     @Override
     public Pose2d getInitialPose() {
-        return new Pose2d(new Translation2d(6.710, 6.950), new Rotation2d(-166.718));
+        return action.getPath().getPathPoses().get(0);
     }
 }
