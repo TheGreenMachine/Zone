7dc8c1ff6b9df293dd3119895f038b8f8c2bfc59
henrymilbert <110063972+henrymilbert@users.noreply.github.com>
2025-03-08 11:57:40 -0600

    get cameras and visible tags onto the normal field and update photonlib version

--- src/main/java/com/team1816/lib/subsystems/vision/Camera.java
+++ src/main/java/com/team1816/lib/subsystems/vision/Camera.java
@@ -10,6 +10,7 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
@@ -36,7 +37,7 @@ public class Camera extends Subsystem{
      * Properties
      */
     private static final String NAME = "camera";
-    private static final List<String> CAMS = List.of("Arducam_OV9281_USB_Camera (1)");
+    private static final List<String> CAMS = List.of("Arducam_OV9281_USB_Camera");
     private static final List<AprilTag> aprilTags = List.of(
             new AprilTag(1, new Pose3d(0, 0, 0.601, new Rotation3d())),
             new AprilTag(2, new Pose3d(0, 2, 0.601, new Rotation3d()))
@@ -96,32 +97,42 @@ public class Camera extends Subsystem{
         robotState.visionEstimatedPoses.clear();
         robotState.visionStdDevs.clear();
         Optional<EstimatedRobotPose> visionEst;
+        List<Pose2d> seenTargetPoses = new ArrayList<>(List.of());
+        boolean hasNewCameraResults = false;
         for (int i = 0; i < cams.size(); i++) {
             for (var change : cams.get(i).getAllUnreadResults()) {
+                hasNewCameraResults = true;
                 visionEst = photonEstimators.get(i).update(change);
                 int finalI = i;
                 visionEst.ifPresent(
                         est -> {
                             robotState.visionEstimatedPoses.add(est);
-                            robotState.visionStdDevs.add(getEstimationStdDevs(est, change.getTargets()));
+                            robotState.visionStdDevs.add(getEstimationStdDevs(est));
                             FieldConfig.field.getObject("vision" + finalI).setPose(est.estimatedPose.toPose2d());
                             if (Constants.kLoggingDrivetrain) {
                                 visionPoseLoggers.get(finalI).append(est.estimatedPose.toPose2d());
                             }
+                            for (PhotonTrackedTarget target : change.getTargets())
+                                seenTargetPoses.add(est.estimatedPose.plus(robotToCams.get(finalI)).plus(target.bestCameraToTarget).toPose2d());
                         });
             }
+            FieldConfig.field.getObject("camera"+i).setPose(
+                    robotState.fieldToVehicle.plus(new Transform2d(robotToCams.get(i).getTranslation().toTranslation2d(), robotToCams.get(i).getRotation().toRotation2d()))
+            );
         }
+        if(hasNewCameraResults)
+            FieldConfig.field.getObject("visibleTargetPoses").setPoses(seenTargetPoses);
     }
 
     /**
      * The standard deviations of the estimated pose from {@link #updateVisionEstimatedPoses()}, for use
-     * with {@link edu.wpi.first.math.estimator.SwerveDrivePoseEstimator SwerveDrivePoseEstimator}.
+     * with {@link SwerveDrivePoseEstimator SwerveDrivePoseEstimator}.
      * This should only be used when there are targets visible.
      *
      * @param estimatedPose The estimated pose to guess standard deviations for.
      */
     public Matrix<N3, N1> getEstimationStdDevs(
-            EstimatedRobotPose estimatedPose, List<PhotonTrackedTarget> targets) {
+            EstimatedRobotPose estimatedPose) {
         // Ignore estimates that are too far off of our current estimate they are
         // probably not correct
         if (Math.abs(robotState.fieldToVehicle.getRotation().getDegrees()
@@ -133,7 +144,7 @@ public class Camera extends Subsystem{
         var estStdDevs = robotState.kSingleTagStdDevs;
         int numTags = 0;
         double avgDist = 0;
-        for (var tgt : targets) {
+        for (var tgt : estimatedPose.targetsUsed) {
             var tagPose = kTagLayout.getTagPose(tgt.getFiducialId());
             if (tagPose.isEmpty()) continue;
             numTags++;
