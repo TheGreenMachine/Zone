e27bef423b94682a47ec196565cb7697d570d85a
EndigoSkull <137455719+EndigoSkull@users.noreply.github.com>
2025-03-18 13:25:24 -0500

    connected coral actions for l4 paths and adjusted the events

--- src/main/java/com/team1816/season/auto/actions/NamedCommandRegistrar.java
+++ src/main/java/com/team1816/season/auto/actions/NamedCommandRegistrar.java
@@ -2,6 +2,8 @@ package com.team1816.season.auto.actions;
 
 import com.pathplanner.lib.auto.NamedCommands;
 import com.team1816.lib.Injector;
+import com.team1816.season.subsystems.CoralArm;
+import com.team1816.season.subsystems.Elevator;
 import com.team1816.season.subsystems.Ramp;
 import edu.wpi.first.wpilibj2.command.Commands;
 
@@ -27,6 +29,18 @@ public final class NamedCommandRegistrar {
 
         NamedCommands.registerCommand("ramp hold l234", Commands.runOnce(() -> {
             Injector.get(Ramp.class).setDesiredState(Ramp.RAMP_STATE.L234_FEEDER);
+        }));
+
+        NamedCommands.registerCommand("coral feeder", Commands.runOnce(() -> {
+            Injector.get(Elevator.class).setDesiredState(Elevator.ELEVATOR_STATE.FEEDER);
+        }));
+
+        NamedCommands.registerCommand("coral l4", Commands.runOnce(() -> {
+            Injector.get(Elevator.class).setDesiredState(Elevator.ELEVATOR_STATE.L4);
+        }));
+
+        NamedCommands.registerCommand("coral outtake", Commands.runOnce(() -> {
+            Injector.get(CoralArm.class).setDesiredIntakeState(CoralArm.INTAKE_STATE.OUTTAKE);
         }));
     }
 }
